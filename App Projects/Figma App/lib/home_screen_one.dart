2c95df0dbad53b0d11de6af23dbc8721f0abc9f6
Hashir Saud Khan <93030144+HashirSaudKhan@users.noreply.github.com>
2024-02-21 09:39:43 +0500

    figma app uploading

---
+++ App Projects/Figma App/lib/home_screen_one.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home_Screen_One extends StatelessWidget {
+  const Home_Screen_One({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Raees BOSS agai Karaingai hoslah rakhaiyai"),
+      ),
+    );
+  }
+}
